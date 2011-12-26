baf5a17fb689262496ed4813426fd3453ebaeed8
Jonas Hietala <tradet.h@gmail.com>
2011-12-26 10:57:14 +0100

    Cleanup ShuffleBag and WeightBag.

--- src/Tree/ShuffleBag.hpp
+++ src/Tree/ShuffleBag.hpp
@@ -22,50 +22,14 @@ namespace Tree
         }
 
         // Take item from rest and place it in bag
-        ShuffleBag &MakeAvailable( T val )
-        {
-            typename List::iterator it = std::find( rest.begin(), rest.end(), val );
-
-            if( it != rest.end() ) {
-                rest.erase( it );
-                bag.push_back( val );
-            }
-
-            return *this;
-        }
+        ShuffleBag &MakeAvailable( T val );
 
         // Return an item, remove from bag and place in rest
         // If bag is empty put back everything from rest in bag
-        T Get()
-        {
-            if( IsEmpty() ) {
-                throw( std::out_of_range( "Shufflebag is empty" ) );
-            }
+        T Get();
 
-            if( bag.empty() ) {
-                bag.swap( rest );
-            }
-
-            int i = Tree::irandom( 0, bag.size() - 1 );
-
-            T item = bag.at(i);
-            rest.push_back( item );
-            bag.erase( bag.begin() + i );
-
-            return item;
-        }
-
-        // Clear bag and rest
-        void Clear()
-        {
-            bag.clear();
-            rest.clear();
-        }
-
-        bool IsEmpty()
-        {
-            return bag.empty() && rest.empty();
-        }
+        void Clear() { bag.clear(); rest.clear(); }
+        bool IsEmpty() { return bag.empty() && rest.empty(); }
 
         typedef std::vector<T> List;
 
@@ -75,6 +39,44 @@ namespace Tree
         List bag;
         List rest;
     };
+
+    /** Template implementations **/
+
+    // Take item from rest and place it in bag
+    template<typename T>
+    ShuffleBag<T> &ShuffleBag<T>::MakeAvailable( T val )
+    {
+        typename List::iterator it = std::find( rest.begin(), rest.end(), val );
+
+        if( it != rest.end() ) {
+            rest.erase( it );
+            bag.push_back( val );
+        }
+
+        return *this;
+    }
+
+    // Return an item, remove from bag and place in rest
+    // If bag is empty put back everything from rest in bag
+    template<typename T>
+    T ShuffleBag<T>::Get()
+    {
+        if( IsEmpty() ) {
+            throw( std::out_of_range( "Shufflebag is empty" ) );
+        }
+
+        if( bag.empty() ) {
+            bag.swap( rest );
+        }
+
+        int i = Tree::irandom( 0, bag.size() - 1 );
+
+        T item = bag.at(i);
+        rest.push_back( item );
+        bag.erase( bag.begin() + i );
+
+        return item;
+    }
 
 }
 
--- src/Tree/WeightBag.hpp
+++ src/Tree/WeightBag.hpp
@@ -12,33 +12,11 @@ namespace Tree
         WeightBag() { }
         ~WeightBag() { }
 
-        WeightBag &Add( float weight, T val )
-        {
-            sum += weight;
-            weights.push_back( sum );
-            vals.push_back( val );
+        WeightBag &Add( float weight, T val );
+        T Get();
 
-            return *this;
-        }
-
-        T Get()
-        {
-            if( weights.empty() ) {
-                throw( std::out_of_range( "Weightbag is empty" ) );
-            }
-
-            float rand = frandom( 0, sum );
-            DP_( "rand", "%.4f", rand );
-            for( size_t i = 0; i < weights.size(); ++i ) {
-                if( rand < weights[i] ) {
-                    return vals[i];
-                }
-            }
-
-            // Should never come here
-            // Only if rand = sum
-            return vals.back();
-        }
+        void Clear() { vals.clear(); weights.clear(); }
+        bool IsEmpty() { return weights.empty(); }
 
         typedef std::vector<T> Vals;
         typedef std::vector<float> Weights;
@@ -50,5 +28,36 @@ namespace Tree
         Weights weights;
         float sum;
     };
+
+    /** Template implementations **/
+
+    template<typename T>
+    WeightBag<T> &WeightBag<T>::Add( float weight, T val )
+    {
+        sum += weight;
+        weights.push_back( sum );
+        vals.push_back( val );
+
+        return *this;
+    }
+
+    template<typename T>
+    T WeightBag<T>::Get()
+    {
+        if( weights.empty() ) {
+            throw( std::out_of_range( "Weightbag is empty" ) );
+        }
+
+        float rand = frandom( 0, sum );
+        for( size_t i = 0; i < weights.size(); ++i ) {
+            if( rand < weights[i] ) {
+                return vals[i];
+            }
+        }
+
+        // Should never come here
+        // Only if rand = sum (which shouldn't happen if we randomize 0 < x < sum)
+        return vals.back();
+    }
 }
 
